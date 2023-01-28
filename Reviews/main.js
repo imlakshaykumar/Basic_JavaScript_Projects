79c8f471e697128f557b0ba31169f94e106551df
Lakshay Kumar <imlakshaykumar@gmail.com>
2023-01-29 02:13:17 +0530

    added change function

--- Reviews/main.js
+++ Reviews/main.js
@@ -7,7 +7,7 @@ const imageArr = [
 
 const nameArr = ['Susan Smith', 'Anna Johanson', 'Peter Jones', 'Bill Anderson'];
 
-const positionArr = ['Web developer', 'Web Designer', 'Intern', 'The Boss'];
+const positionArr = ['Web Developer', 'Web Designer', 'Intern', 'The Boss'];
 
 const descriptionArr = [
     "Im baby meggings twee health goth + 1. Bicycle rights tumeric chartreuse before they sold out chambray pop - up.Shaman humblebrag pickled coloring book salvia hoodie, cold - pressed four dollar toast everyday carry",
@@ -33,8 +33,30 @@ const imageEL = document.querySelector("#image-el");
 const nameEL = document.querySelector("#name-el");
 const positionEL = document.querySelector("#position-el");
 const descriptionEL = document.querySelector("#description-el");
+const leftBtn = document.querySelector("#left-iconBtn")
+const rightBtn = document.querySelector("#right-iconBtn")
 
 const surpriseBtn = document.querySelector("#surprise-Btn");
+
+let arrIndex;
+let image;
+let fullName;
+let position;
+let description;
+function change() {
+    imageEL.src = imageArr[arrIndex];
+    nameEL.textContent = nameArr[arrIndex];
+    positionEL.textContent = positionArr[arrIndex];
+    descriptionEL.textContent = descriptionArr[arrIndex];
+}
+
+leftBtn.addEventListener('click', () => {
+
+})
+
+rightBtn.addEventListener('click', () => {
+
+})
 
 surpriseBtn.addEventListener('click', () => {
     // console.log("Hello")
@@ -42,10 +64,10 @@ surpriseBtn.addEventListener('click', () => {
     let randomNum = randomNumber();
     console.log(randomNum);
 
-    let image = imageArr[randomNum];
-    let fullName = nameArr[randomNum];
-    let position = positionArr[randomNum];
-    let description = descriptionArr[randomNum];
+    image = imageArr[randomNum];
+    fullName = nameArr[randomNum];
+    position = positionArr[randomNum];
+    description = descriptionArr[randomNum];
 
     imageEL.src = image;
     nameEL.textContent = fullName;
